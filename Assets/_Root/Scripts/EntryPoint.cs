ca31c604085f3f761e7e351bfe75f3986676e89f
Valkirin88 <91696211+Valkirin88@users.noreply.github.com>
2022-08-11 21:11:03 +0300

    HW3_task1

---
+++ Assets/_Root/Scripts/EntryPoint.cs
@@ -0,0 +1,25 @@
+using Profile;
+using UnityEngine;
+
+internal class EntryPoint : MonoBehaviour
+{
+    private const float SpeedCar = 15f;
+    private const float JumpHeight = 2f;
+    private const GameState InitialState = GameState.Start;
+
+    [SerializeField] private Transform _placeForUi;
+
+    private MainController _mainController;
+
+
+    private void Start()
+    {
+        var profilePlayer = new ProfilePlayer(SpeedCar, JumpHeight, InitialState);
+        _mainController = new MainController(_placeForUi, profilePlayer);
+    }
+
+    private void OnDestroy()
+    {
+        _mainController.Dispose();
+    }
+}
---
+++ Assets/_Root/Scripts/Profile/ProfilePlayer.cs
@@ -0,0 +1,26 @@
+using Tool;
+using Game.Car;
+using Features.Inventory;
+
+namespace Profile
+{
+    internal class ProfilePlayer
+    {
+        public readonly SubscriptionProperty<GameState> CurrentState;
+        public readonly CarModel CurrentCar;
+        public readonly InventoryModel Inventory;
+
+
+        public ProfilePlayer(float speedCar, float jumpHeight, GameState initialState) : this(speedCar, jumpHeight)
+        {
+            CurrentState.Value = initialState;
+        }
+
+        public ProfilePlayer(float speedCar, float jumpHeight)
+        {
+            CurrentState = new SubscriptionProperty<GameState>();
+            CurrentCar = new CarModel(speedCar, jumpHeight);
+            Inventory = new InventoryModel();
+        }
+    }
+}
